f44a93b72ae6c3ffb0f017adfe55c0bb038448f2
Kevin Keyser <Kevin.Keyser@live.com>
2016-06-08 01:27:02 -0700

    Added Camera and DebugViewer

---
+++ PhysK/PhysK Sample/PhysK Sample/Camera.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using PhysK;
+
+namespace PhysKSample
+{
+    public class Camera
+    {
+        private static Camera main;
+
+        public static Camera Main
+        {
+            get { return main; }
+            private set { main = main??value; }
+        }
+
+        public GraphicsDevice GraphicsDevice { get; private set; }
+
+        private Vector2 position;
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set
+            {
+                position = value;
+                UpdateView();
+            }
+        }
+
+        private float rotation;
+
+        public float Rotation
+        {
+            get { return rotation; }
+            set
+            {
+                rotation = value; 
+                UpdateView();
+            }
+        }
+
+        public Matrix View { get; private set; }
+        
+        private Vector2 scale;
+
+        public Vector2 Scale
+        {
+            get { return scale; }
+            set
+            {
+                scale = value; 
+                UpdateProjection();
+            }
+        }
+
+        public Matrix Projection { get; private set; }
+
+        public Camera(GraphicsDevice graphicsDevice)
+        {
+            GraphicsDevice = graphicsDevice;
+            position = Vector2.Zero;
+            rotation = 0f;
+            scale = Vector2.One;
+            UpdateView();
+            UpdateProjection();
+        }
+
+        private void UpdateView()
+        {
+            View = Matrix.CreateLookAt(position.ToVector3(-1), position.ToVector3(), MathUtils.GetUnitCircle(rotation - MathHelper.PiOver2).ToVector3());
+        }
+
+        private void UpdateProjection()
+        {
+            Projection = Matrix.CreateOrthographic(GraphicsDevice.Viewport.Width * scale.X, GraphicsDevice.Viewport.Height * scale.Y, 0, 10);
+        }
+    }
+}
---
+++ PhysK/PhysK/PhysK/DebugView.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PhysK
+{
+    public class DebugView
+    {
+        private static VertexPositionColor[] circleVertexPositionColors;
+        private static Dictionary<Shape, VertexPositionColor[]> vertexPositionColors;
+
+        private readonly GraphicsDevice graphicsDevice;
+
+        private readonly BasicEffect basicEffect;
+
+        private World world;
+
+        public World World
+        {
+            get { return world; }
+            set { world = value; }
+        }
+
+        public DebugView(GraphicsDevice graphicsDevice, World world)
+        {
+            if (circleVertexPositionColors == null)
+            {
+                circleVertexPositionColors = new VertexPositionColor[100];
+                for (int i = 0; i < circleVertexPositionColors.Length; i++)
+                {
+                    circleVertexPositionColors[i] = new VertexPositionColor(MathUtils.GetUnitCircle(MathHelper.TwoPi * i / circleVertexPositionColors.Length).ToVector3(), Color.Red);
+                }
+
+                vertexPositionColors = new Dictionary<Shape, VertexPositionColor[]>();
+            }
+            this.graphicsDevice = graphicsDevice;
+            basicEffect = new BasicEffect(graphicsDevice)
+            {
+                VertexColorEnabled = true
+            };
+            World = world;
+        }
+
+        public void Draw(Matrix view, Matrix projection)
+        {
+            basicEffect.View = view;
+            basicEffect.Projection = projection;
+
+            foreach (Particle particle in world.Items)
+            {
+                if (particle is Rigidbody)
+                {
+                    if ((particle as Rigidbody).Shape is Circle)
+                    {
+                        basicEffect.World = Matrix.CreateScale((particle as Rigidbody).Shape.AABB.GetSize().ToVector3() / 2) *
+                                            Matrix.CreateFromYawPitchRoll(0, 0, (particle as Rigidbody).Rotation) *
+                                            Matrix.CreateTranslation(particle.Position.ToVector3());
+                        basicEffect.CurrentTechnique.Passes[0].Apply();
+                        graphicsDevice.DrawUserPrimitives(PrimitiveType.LineStrip, circleVertexPositionColors, 0, circleVertexPositionColors.Length - 1);
+                    }
+                    else
+                    {
+                        if (!vertexPositionColors.ContainsKey((particle as Rigidbody).Shape))
+                        {
+                            VertexPositionColor[] vertices = new VertexPositionColor[(particle as Rigidbody).Shape.Vertices.Length];
+                            for (int i = 0; i < (particle as Rigidbody).Shape.Vertices.Length; i++)
+                            {
+                                vertices[i] = new VertexPositionColor((particle as Rigidbody).Shape.Vertices[i].ToVector3(), Color.Red);
+                            }
+                        }
+                        basicEffect.World = Matrix.CreateScale((particle as Rigidbody).Shape.AABB.GetSize().ToVector3()) *
+                                            Matrix.CreateFromYawPitchRoll(0, 0, (particle as Rigidbody).Rotation) *
+                                            Matrix.CreateTranslation(particle.Position.ToVector3());
+                        basicEffect.CurrentTechnique.Passes[0].Apply();
+                        graphicsDevice.DrawUserPrimitives(PrimitiveType.LineStrip, vertexPositionColors[(particle as Rigidbody).Shape], 0, vertexPositionColors[(particle as Rigidbody).Shape].Length - 1);
+                    }
+                }
+                else
+                {
+                    basicEffect.World = Matrix.CreateTranslation(particle.Position.ToVector3());
+                    basicEffect.CurrentTechnique.Passes[0].Apply();
+                    graphicsDevice.DrawUserPrimitives(PrimitiveType.LineStrip, circleVertexPositionColors, 0, circleVertexPositionColors.Length - 1);
+                }
+            }
+        }
+    }
+}
